a45762100504e2bd024b8436a531e3ca8c45ec28
parvathyjnair01 <parvathyj06@gmail.com>
2024-10-19 14:51:36 +0530

    Add files via upload

---
+++ arithmeticoperators.py
@@ -0,0 +1,13 @@
+'''Author:Parvathy J Nair
+Date:19-10-2024
+Description:Simple desktop calculator using Python.
+Only the five basic arithmetic operators.'''
+number1=int(input("Enter num 1:"))
+number2=int(input("Enter num 2:"))
+number3=int(input("Enter num 3:"))
+print("The sum of num1 and num2 is:",number1+number2)
+print("The difference when num1 is subtracted from num2:",number1-number2)
+print("The product of num1 and num2 is:",number1*number2)
+print("The quotient when num1 is divided by num2 is:",number1/number2)
+print("The remainder when num1 is divided by num2 is:",number1%number2)
+print("The result of (num1 + num2) * num3 / 2 is: ",(number1+number2)*number3/2)
