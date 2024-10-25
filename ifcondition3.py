66b89a75b8fdba262c6085ea507db4e58ac06b84
parvathyjnair01 <parvathyj06@gmail.com>
2024-10-25 14:48:02 +0530

    Add files via upload

---
+++ ifcondition3.py
@@ -0,0 +1,5 @@
+num1=int(input("Enter First number:"))
+num2=int(input("Enter Second number"))
+num3=int(input("Enter Third number:"))
+if num3>num1 and num3>num2:
+    print("The largest number is:",num3)
