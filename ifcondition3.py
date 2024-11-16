65a9efa356612c0833f455cfebb79e93dee767f6
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-16 16:34:06 +0530

    Delete ifcondition3.py

--- ifcondition3.py
+++
@@ -1,5 +0,0 @@
-num1=int(input("Enter First number:"))
-num2=int(input("Enter Second number"))
-num3=int(input("Enter Third number:"))
-if num3>num1 and num3>num2:
-    print("The largest number is:",num3)
