1035bed22d24e3b02f2923af0129247b24ae2f22
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-16 16:54:14 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,34 @@
+#print("hello_world")
+#print ("Hello\nWorld")
+"""if 5 > 3:
+ print("five is greater than three")
+if 5 > 3 :
+    print("five is greater than three")
+    print("five greter than three")"""
+x=22
+y="Parvathy"
+print(x)
+print(y)
+x=5
+x="Jayakumar"
+print(type(x))
+x=int(3)
+y=float(3.5)
+print(type(x))
+print(type(y))
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+x="Python "
+y="is "
+z="awsome"
+print(x + y + z)
+username=input("enter username:")
+print("username is",username)
+a="Hello"
+b="World"
+c=a+b
+print(c)
+a=("hello ,world")
+print(a[1])
