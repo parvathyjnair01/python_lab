f8aefc2641d079fb8b3998bfb016445101e10b34
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-22 16:02:06 +0530

    Add files via upload

---
+++ patterns_of_stars.py
@@ -0,0 +1,30 @@
+'''
+Author:Parvathy J Nair'''
+limit=int(input("Enter number of rows:"))
+for i in range(1,limit+1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+limit=int(input("Enter number of rows:"))
+for i in range(limit,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+limit=int(input("Enter number of rows:"))
+for i in range(1,limit+1):
+    for j in range(limit-i):
+        print(" ",end=" ")
+    for k in range(2*i-1):
+        print("*",end=" ")
+    print()
+limit=int(input("Enter number of rows:"))
+for i in range(limit,0,-1):
+    for j in range(limit-i):
+            print(" ",end=" ")
+    for k in range(2*i-1):
+        print("*",end=" ")
+    print()
+
+
