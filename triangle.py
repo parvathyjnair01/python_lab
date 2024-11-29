35909d5ffe1ebc7e7483fa54ea5cf0071e05d8b3
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-29 15:55:23 +0530

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -5,11 +5,13 @@ The program should output whether or not the triangle is a right triangle.
  Implement using functions.
  '''
 def right_triangle():
-    a=int(input("Enter hypotenuse side:"))
-    b=int(input("Enter base side:"))
-    c=int(input("Enter altitude side:"))
-    if a*a==b*b+c*c:
+    a=int(input("Enter side-1:"))
+    b=int(input("Enter side-2:"))
+    c=int(input("Enter side-3:"))
+    side=[a,b,c]
+    side.sort()
+    if side[2]**2==side[1]**2+side[0]**2:
         print ("Right angled triangle")
     else:
         print("Not right angled")
-right_triangle() 
+right_triangle()
