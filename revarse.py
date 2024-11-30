37eb1cf0add3a63ce5d052221e60afcaceea4a31
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-30 16:07:26 +0530

    Add files via upload

---
+++ revarse.py
@@ -0,0 +1,9 @@
+'''
+Author:Parvathy J Nair
+Description:Write a Python program to reverse a string
+'''
+def reverse():
+    sample_string="1234abcd"
+    print(sample_string[0:])
+    print(sample_string[-1::-1])
+reverse()
