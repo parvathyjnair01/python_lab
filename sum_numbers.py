873a58fb56f88f9a57e41b6f3e464c6daa90c6cc
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-30 15:13:00 +0530

    Add files via upload

---
+++ sum_numbers.py
@@ -0,0 +1,9 @@
+'''
+Author:Parvathy J Nair
+Description:Write a Python function to sum all the numbers in a list
+'''
+
+def sum_of_numbers():
+    list=[1,2,3,4]
+    print(sum(list))
+sum_of_numbers()
